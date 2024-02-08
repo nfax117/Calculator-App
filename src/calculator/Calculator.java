4ed05087321413367a8eb3714191376ece51d224
nfax117 <74328491+nfax117@users.noreply.github.com>
2024-02-07 16:54:53 -0800

    Initial Push from desktop

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Nfax1
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
